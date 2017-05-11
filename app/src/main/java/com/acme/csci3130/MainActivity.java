a730044474740fc0878f8a139fb341b0d1bbd193
Jung-Soo Lee <Jung-Soo Lee>
2017-05-10 23:40:13 -0300

    First Commit

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,12 +2,30 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+    EditText copyText;
+    TextView txtresult;
+    TextView textView2;
+    Button bt1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtresult = (TextView)findViewById(R.id.helloText);
+        textView2 = (TextView)findViewById(R.id.textView2);
+        copyText = (EditText)findViewById(R.id.editText);
+        bt1 = (Button)findViewById(R.id.bt1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtresult.setText(copyText.getText().toString());
+
+            }
+        });
     }
 }
